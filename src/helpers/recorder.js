f18616d2bac648c9d4aac844cb4147bc51cd480f
Efrain Rochin <erochin@grupoadhoc.mx>
2018-11-07 17:34:58 -0700

    Quita la carpeta de la fecha

--- src/helpers/recorder.js
+++ src/helpers/recorder.js
@@ -5,148 +5,143 @@
 //  Created by Sahil Chaddha on 24/08/2018.
 //
 
-const moment = require('moment')
-const childProcess = require('child_process')
-const path = require('path')
-const FileHandler = require('./fileHandler')
+const moment = require('moment');
+const childProcess = require('child_process');
+const path = require('path');
+const FileHandler = require('./fileHandler');
 
-const fh = new FileHandler()
+const fh = new FileHandler();
 
 const RTSPRecorder = class {
   constructor(config = {}) {
-    this.config = config
-    this.name = config.name
-    this.url = config.url
-    this.timeLimit = config.timeLimit || 60
-    this.folder = config.folder || 'media/'
-    this.categoryType = config.type || 'video'
-    fh.createDirIfNotExists(this.getDirectoryPath())
-    fh.createDirIfNotExists(this.getTodayPath())
+    this.config = config;
+    this.name = config.name;
+    this.url = config.url;
+    this.timeLimit = config.timeLimit || 60;
+    this.folder = config.folder || 'media/';
+    this.categoryType = config.type || 'video';
+    fh.createDirIfNotExists(this.getDirectoryPath());
   }
 
   getDirectoryPath() {
-    return path.join(this.folder, (this.name ? this.name : ''))
-  }
-
-  getTodayPath() {
-    return path.join(this.getDirectoryPath(), moment().format('MMM-Do-YY'))
+    return path.join(this.folder, (this.name ? this.name : ''));
   }
 
   getMediaTypePath() {
-    return path.join(this.getTodayPath(), this.categoryType)
+    return path.join(this.getDirectoryPath(), this.categoryType);
   }
 
   getFilename(folderPath) {
-    return path.join(folderPath, moment().format() + this.getExtenstion())
+    return path.join(folderPath, moment().format() + this.getExtenstion());
   }
 
   getExtenstion() {
     if (this.categoryType === 'audio') {
-      return '.avi'
+      return '.avi';
     }
     if (this.categoryType === 'image') {
-      return '.jpg'
+      return '.jpg';
     }
 
-    return '.mp4'
+    return '.mp4';
   }
 
   getArguments() {
     if (this.categoryType === 'audio') {
-      return ['-vn', '-acodec', 'copy']
+      return ['-vn', '-acodec', 'copy'];
     }
     if (this.categoryType === 'image') {
-      return ['-vframes', '1']
+      return ['-vframes', '1'];
     }
-    return ['-acodec', 'copy', '-vcodec', 'copy']
+    return ['-acodec', 'copy', '-vcodec', 'copy'];
   }
 
   getChildProcess(fileName) {
-    var args = ['-i', this.url]
-    const mediaArgs = this.getArguments()
+    var args = ['-i', this.url];
+    const mediaArgs = this.getArguments();
     mediaArgs.forEach((item) => {
-      args.push(item)
-    })
-    args.push(fileName)
+      args.push(item);
+    });
+    args.push(fileName);
     return childProcess.spawn('ffmpeg',
       args,
-      { detached: false, stdio: 'ignore' })
+      { detached: false, stdio: 'ignore' });
   }
 
   stopRecording() {
-    this.disableStreaming = true
+    this.disableStreaming = true;
     if (this.timer) {
-      clearTimeout(this.timer)
-      this.timer = null
+      clearTimeout(this.timer);
+      this.timer = null;
     }
     if (this.writeStream) {
-      this.killStream()
+      this.killStream();
     }
   }
 
   startRecording() {
     if (!this.url) {
-      console.log('URL Not Found.')
-      return true
+      console.log('URL Not Found.');
+      return true;
     }
-    this.recordStream()
+    this.recordStream();
   }
 
   captureImage(cb) {
-    this.writeStream = null
-    const folderPath = this.getMediaTypePath()
-    fh.createDirIfNotExists(folderPath)
-    const fileName = this.getFilename(folderPath)
-    this.writeStream = this.getChildProcess(fileName)
+    this.writeStream = null;
+    const folderPath = this.getMediaTypePath();
+    fh.createDirIfNotExists(folderPath);
+    const fileName = this.getFilename(folderPath);
+    this.writeStream = this.getChildProcess(fileName);
     this.writeStream.once('exit', () => {
       if (cb) {
-        cb()
+        cb();
       }
-    })
+    });
   }
 
   killStream() {
-    this.writeStream.kill()
+    this.writeStream.kill();
   }
 
   recordStream() {
     if (this.categoryType === 'image') {
-      return
+      return;
     }
-    const self = this
+    const self = this;
     if (this.timer) {
-      clearTimeout(this.timer)
+      clearTimeout(this.timer);
     }
 
     if (this.writeStream && this.writeStream.binded) {
-      return false
+      return false;
     }
 
     if (this.writeStream && this.writeStream.connected) {
-      this.writeStream.binded = true
+      this.writeStream.binded = true;
       this.writeStream.once('exit', () => {
-        self.recordStream()
-      })
-      this.killStream()
-      return false
+        self.recordStream();
+      });
+      this.killStream();
+      return false;
     }
 
-    this.writeStream = null
-    const folderPath = this.getMediaTypePath()
-    fh.createDirIfNotExists(folderPath)
-    const fileName = this.getFilename(folderPath)
-    this.writeStream = this.getChildProcess(fileName)
+    this.writeStream = null;
+    const folderPath = this.getMediaTypePath();
+    fh.createDirIfNotExists(folderPath);
+    const fileName = this.getFilename(folderPath);
+    this.writeStream = this.getChildProcess(fileName);
 
     this.writeStream.once('exit', () => {
       if (self.disableStreaming) {
-        return true
+        return true;
       }
-      self.recordStream()
-    })
-    this.timer = setTimeout(self.killStream.bind(this), this.timeLimit * 1000)
+      self.recordStream();
+    });
+    this.timer = setTimeout(self.killStream.bind(this), this.timeLimit * 1000);
 
-    console.log('Start record ' + fileName)
+    console.log('Start record ' + fileName);
   }
-}
+};
 
-module.exports = RTSPRecorder
+module.exports = RTSPRecorder;
